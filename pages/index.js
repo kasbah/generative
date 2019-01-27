f55187e6d66b7c3751b08e83f5fbba6c324a9721
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2019-01-27 00:06:12 +0000

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Link from "next/link"
+
+export default () => (
+  <div>
+    <Link href="/squares">
+      <a>squares</a>
+    </Link>{" "}
+  </div>
+)
---
+++ pages/squares.js
@@ -0,0 +1,68 @@
+import React, {Component} from "react"
+import _ from "lodash"
+
+class App extends Component {
+  render() {
+    let x = 11
+    let y = 11
+    let size = 50
+    let border = 10
+    let gap = 5
+    let width = border * 2 + x * (size + gap)
+    let height = border * 2 + y * (size + gap)
+    let numberOfInnerSquares = 5
+    let reduction = size / numberOfInnerSquares
+    return (
+      <div className="App">
+        <svg viewBox={`0 0 ${width} ${height}`} width={width} height={height}>
+          <Squares
+            x={x}
+            y={y}
+            size={size}
+            border={border}
+            gap={gap}
+            reduction={reduction}
+            numberOfInnerSquares={numberOfInnerSquares}
+          />
+        </svg>
+      </div>
+    )
+  }
+}
+
+function Squares(props) {
+  return _.range(props.x).map(i => {
+    let ir = Math.random()
+    return _.range(props.y).map(j => {
+      let jr = Math.random()
+      return _.range(props.numberOfInnerSquares).map(k => {
+        let kr = Math.random()
+        return (
+          <rect
+            width={props.size - props.reduction * k}
+            height={props.size - props.reduction * k}
+            x={
+              props.border +
+              (props.reduction / 2) * k +
+              i * (props.size + props.gap) +
+              jr * 10
+            }
+            y={
+              props.border +
+              (props.reduction / 2) * k +
+              j * (props.size + props.gap) +
+              ir * 10
+            }
+            style={{
+              fill: "rgba(0, 0, 0, 0)",
+              strokeWidth: kr * 6,
+              stroke: ir * jr > 0.8 ? "#FD5F00" : "black"
+            }}
+          />
+        )
+      })
+    })
+  })
+}
+
+export default App
