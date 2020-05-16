b615f5b6a05d719f7cc847b088852f4d6a102fa5
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2020-05-16 06:53:00 +0100

    Make basic constraints example work

--- pages/constraints.js
+++ pages/constraints.js
@@ -6,19 +6,37 @@ import {
   Tool,
 } from "@psychobolt/react-paperjs"
 
+const LENGTH = 50
+
 const App = ({paper}) => {
   const [mousePos, setMousePos] = React.useState(paper.view.center)
+  const [ballPos, setBallPos] = React.useState(paper.view.center)
   return (
-    <div>
-      <Tool
-        onMouseMove={({event}) => {
-          setMousePos([event.clientX, event.clientY])
-        }}
-      >
-        <Circle center={mousePos} radius={35} fillColor="red" />
-      </Tool>
-    </div>
+    <Tool
+      onMouseMove={({event}) => {
+        const mouse = new paper.Point(event.clientX, event.clientY)
+        setMousePos(mouse)
+        setBallPos((pos) => constrainDistance(pos, mouse, LENGTH))
+      }}
+    >
+      <Circle
+        center={mousePos}
+        radius={LENGTH}
+        strokeWidth={1}
+        strokeColor="black"
+      />
+      <Circle
+        center={ballPos}
+        radius={5}
+        strokeWidth={10}
+        strokeColor="black"
+      />
+    </Tool>
   )
+}
+
+function constrainDistance(point, anchor, distance) {
+  return point.subtract(anchor).normalize().multiply(distance).add(anchor)
 }
 
 export default (props) => {
@@ -26,7 +44,7 @@ export default (props) => {
     <PaperContainer
       canvasProps={{
         resize: "true",
-        style: {width: "100%", height: "100vh", cursor: "none"},
+        style: {width: "100%", height: "99.2vh", cursor: "none"},
       }}
     >
       {renderWithPaperScope((paper) => (
